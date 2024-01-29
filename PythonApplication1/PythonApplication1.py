64bf398a0d467f794afdcffb1ff6146e6cf7b3e0
opilane <opilane@HP-CZC2349HTQ>
2024-01-29 10:38:41 +0200

    sa

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -160,19 +160,24 @@
 #    print(f"{i:2} {ruut:2} {kuup:3}")
 
 
-from random import *
-from datetime import *
+#from random import *
+#from datetime import *
 
-summa = float(input("Напишите какую сумму вы хотите положить"))
-test_summa = summa
-procent = randint(1,10)
-print("Ты сделал взнос", summa, "Твой процент равен", procent)
+#summa = float(input("Напишите какую сумму вы хотите положить"))
+#test_summa = summa
+#procent = randint(1,10)
+#print("Ты сделал взнос", summa, "Твой процент равен", procent)
 
-howmanyear = int(input("На сколько лет"))
-for i in range(1, howmanyear+1):
-    intsumma = (summa + procent) 
-    test_summa = summa + intsumma
-    print(f"{i} {summa} {intsumma} {test_summa}" )
-    summa + test_summa
-print(f"Сумма итог: {test_summa} eur")
-print(f"Выгода: {test_summa - summa} eur" )
+#howmanyear = int(input("На сколько лет"))
+#for i in range(1, howmanyear+1):
+#    intsumma = (summa + procent) 
+#    test_summa = summa + intsumma
+#    print(f"{i} {summa} {intsumma} {test_summa}" )
+#    summa + test_summa
+#print(f"Сумма итог: {test_summa} eur")
+#print(f"Выгода: {test_summa - summa} eur" )
+for j in range(1, 11):
+    for i in range(1, 11):
+        print("{} ".format(i * j), end="")
+    print()
+
