7bb731dbcbc63d933f7d878798620610c76b54af
Artemij Issajenko <93095044+ArtemijIssajenko001@users.noreply.github.com>
2024-01-30 10:50:41 +0200

    Update kontroltoo.py

--- PythonApplication1/kontroltoo.py
+++ PythonApplication1/kontroltoo.py
@@ -1,6 +1,6 @@
 from random import *
 from datetime import *
-###1
+#1
 try:
     n = int(input("Kirjutage 1-9")) 
     if n == "1, 2, 3, 4, 5, 6, 7, 8, 9": 
@@ -11,35 +11,38 @@ try:
 except :
     print("kasutage ainult numbreid")
 
-###2
+#2
 
-#n = int(input("N"))
-#for i in range(n**3):
+n = int(input("n: "))
+for i in range(n**3 + 1):
+    result = n ** i
+    if result <= n ** 3:
+        print(f"{n} в степени {i} равно {result}")
+    else:
+        break
 
-#   print(i)
+#3
 
-###3
+from random import *
 
-##from random import *
+o = randint(10, 31)  #õpetused
+s = 0 
 
-#o = randint(10, 31)  #õpetused
-#s = 0 
+for i in range(o):
+    g = randint(1, 5) #hinnangud
+    s+=g  #siin võtan hinnangud kokku
+a = s / o 
+g1 = round(a, 2)
+print("Õpilaste keskmine hinne =", g1)
 
-#for i in range(o):
-#    g = randint(1, 5) #hinnangud
-#    s+=g  #siin võtan hinnangud kokku
-#a = s / o 
-#g1 = round(a, 2)
-#print("Õpilaste keskmine hinne =", g1)
-
-###4
-esimine = 5 #esimine summ
-aeg = 0 #aeg 
+#4
+esimine = 5 #первая сумма
+aeg = 0 #возвраст 
 rand = randint(1,8)
 
 for t in range(rand):
     esimine = esimine * 2 + aeg   
     aeg += 1
     t += esimine  
-    print(f"Aeg: {aeg}, Kingitus: {esimine}, Kogusumma: {t}") #vastus
+    print(f"Aeg: {aeg}, Kingitus: {esimine}, Kogusumma: {t}") #ответ
 print(f" {aeg} lõplik vanus.")
